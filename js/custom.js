7ce3a39804a63689aee2295dbf336023c53727d5
sarthak1009 <sarthakdubey1009@gmail.com>
2018-09-13 21:56:26 +0530

    Services updated

---
+++ js/custom.js
@@ -0,0 +1,10 @@
+var g = getElementByClassName('white-right');
+var h = getElementByClassName('srvclr');
+
+(function(){
+ getElementByClassName('white-right').mouseover(function() {
+    getElementByClassName('srvclr').color = "red";
+  });
+}());
+
+ 
